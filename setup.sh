4a4d727de9d467a98a3c1741f117ac14b811b22f
manuylo <55402063+manuylo@users.noreply.github.com>
2022-09-20 16:06:06 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+echo "[server]"  >> ~/.streamlit/config.toml
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
+echo "[global]"  >> ~/.streamlit/config.toml
+echo "developmentMode = false"  >> ~/.streamlit/config.toml
